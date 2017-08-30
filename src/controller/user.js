dd0c5fe12ee0fab6b42a5a8ffbfa1465d9414d7c
klwf <klwfwdk@qq.com>
2017-08-30 20:53:20 +0800

    text

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,8 +1,15 @@
 const Base = require('./base.js');
 
 module.exports = class extends Base {
+  __before() {
+    console.log("hello");
+    console.log(this.ctx.url)
+  }
   indexAction() {
-    this.body='hello world';
+    this.body = 'hello world';
     console.log('hello world');
+  }
+  _call() {
+    this.body = '不存在'
   }
 };
