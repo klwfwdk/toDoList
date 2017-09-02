fe7f4a4f67009252741fa880d3030cdf75118a5f
klwf <klwfwdk@qq.com>
2017-09-02 22:47:39 +0800

    www

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,8 +1,21 @@
 const Base = require('./base.js');
-module.exports = class extends Base {
-  indexAction() {
-  this.body="hello"
-  console.log('ewee');
-   handle:'app';
+module.exports = class extends think.Controller {
+  //login action
+  async loginAction(){
+    import React from 'react';
+    import ReactDOM from 'react-dom';
+    import GitHubLogin from 'react-github-login';
+     
+    const onSuccess = response => console.log(response);
+    const onFailure = response => console.error(response);
+     
+    ReactDOM.render(
+      <GitHubLogin clientId="b7a21a56f032455afa67"
+    redirectUri="/login/index"
+    buttonText="login"
+        onSuccess={onSuccess}
+        onFailure={onFailure}/>,
+      document.getElementById('example')
+    );
   }
-};
+}
