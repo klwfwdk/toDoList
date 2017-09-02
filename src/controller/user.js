6e83a062c2642e82f18edcc50146d3402d8c0005
klwf <klwfwdk@qq.com>
2017-09-02 11:19:26 +0800

    www

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,10 +1,8 @@
 const Base = require('./base.js');
-const koa = require('koa')
-const app = require('app')
 module.exports = class extends Base {
   indexAction() {
-  
-  handle:'app';
-  
+  this.body="hello"
+  console.log('ewee');
+   handle:'app';
   }
 };
