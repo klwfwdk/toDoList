ec85c5b93860d288126a412241c84c08d6ebec9f
klwf <klwfwdk@qq.com>
2017-09-03 20:55:39 +0800

    2w

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,18 +1,24 @@
 const Base = require('./base.js');
 module.exports = class extends think.Controller {
   //login action
-  async loginAction(){
-      let path = "http://github.com/login/oauth/authorize";
-      path += '?client_id=' + 'b7a21a56f032455afa67';
-     path += '&scope='+'user:email';
-      path += '&state='+ 'dataStr';
-      //转发到授权服务器
-     this.redirect(path);
+  async loginAction() {
+    let path = "http://github.com/login/oauth/authorize";
+    path += '?client_id=' + 'b7a21a56f032455afa67';
+    path += '&scope=' + 'user:email';
+    path += '&state=' + 'dataStr';
+    //转发到授权服务器
+    this.redirect(path);
   };
-  async tokenAction(){
-   var code= this.ctx.param('code');
-   console.log(code);
-  var path = "https://api.github.com/user?access_token="+code;
-   this.redirect(path);
-  }
-  }
+  async PostAction() {
+    var querystring = require('querystring'); 
+    var request= require('request');   
+    let coded = this.ctx.param('code');
+    let data =from({
+      client_id: "b7a21a56f032455afa67",
+      client_secret: "c29bd25a4543859e69bb8b68b2e30afe705e98bf",
+      code:coded,
+      redirect_uri:"/"
+    })
+request.post('https://github.com/login/oauth/access_token').data;
+}
+}
