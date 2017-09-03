a383e62ed39a1dd0e6edc2b2c6cbd2479582f8ec
klwf <klwfwdk@qq.com>
2017-09-03 08:04:42 +0800

    222

--- src/controller/user.js
+++ src/controller/user.js
@@ -2,9 +2,9 @@ const Base = require('./base.js');
 module.exports = class extends think.Controller {
   //login action
   async loginAction(){
-      var path = "https://github.com/login/OAuth/authorize";
+      var path = "http://github.com/login/oauth/authorize";
       path += '?client_id=' + 'b7a21a56f032455afa67';
-     // path += '&scope='+OAuthConfig.GITHUB_CLIENT_SCOPE;
+     path += '&scope='+'user:email';
       path += '&state='+ 'dataStr';
       //转发到授权服务器
      this.redirect(path);
