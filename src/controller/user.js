1a2a84eaebba0ebbec004b2e1fe977b1e0d9abb6
klwf <klwfwdk@qq.com>
2017-09-03 08:33:15 +0800

    2w

--- src/controller/user.js
+++ src/controller/user.js
@@ -2,11 +2,16 @@ const Base = require('./base.js');
 module.exports = class extends think.Controller {
   //login action
   async loginAction(){
-      var path = "http://github.com/login/oauth/authorize";
+      let path = "http://github.com/login/oauth/authorize";
       path += '?client_id=' + 'b7a21a56f032455afa67';
      path += '&scope='+'user:email';
       path += '&state='+ 'dataStr';
       //转发到授权服务器
      this.redirect(path);
   };
+  async tokenAction(){
+   var code= ctx.post('code');
+   var path = "https://api.github.com/user?access_token="+code;
+   this.redirect(path);
+  }
   }
