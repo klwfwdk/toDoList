2d7418c630c7faa5ec2d10f59cc4fb98da379f78
klwf <klwfwdk@qq.com>
2017-09-04 15:09:54 +0800

    2w

--- src/controller/user.js
+++ src/controller/user.js
@@ -7,7 +7,7 @@ module.exports = class extends think.Controller {
   //login action
   async loginAction() {
 
-    /*let path = "http://github.com/login/oauth/authorize";
+    let path = "http://github.com/login/oauth/authorize";
    path += '?client_id=' + 'b7a21a56f032455afa67';
    path += '&scope=' + 'user:email';
    path += '&state=' + 'dataStr';
@@ -16,11 +16,9 @@ module.exports = class extends think.Controller {
 
  };
  async PostAction() {
-   var querystring = require('querystring');
-   var request = require('request');
    let coded = this.ctx.param('code');
   // console.log(coded);
-   var k = request.post('https://github.com/login/oauth/access_token', {
+   var k = request.get('https://github.com/login/oauth/access_token', {
      from: {
        client_id: "b7a21a56f032455afa67",
        client_secret: "c29bd25a4543859e69bb8b68b2e30afe705e98bf",
@@ -28,30 +26,13 @@ module.exports = class extends think.Controller {
      }
    }, function (error, response, body) {
      if (!error) {
-       console.log(this.ctx.post());
+       console.log("elll");
      }
    }
    );
   
- }*/
+ 
     
-    var url = 'http://github.com/login/oauth/authorize';
-      url +="?client_id="+CONSUMER_KEY;
-      //url +="?scope="+"user:email";
-      ;
-      this.redirect(url);
-  }
-  async PostAction(){
-    let coded = this.ctx.param('code');
-    var oauth={
-      client_id: "b7a21a56f032455afa67",
-      client_secret: "c29bd25a4543859e69bb8b68b2e30afe705e98bf",
-      code: coded
-    }
-    request.post({url:'https://github.com/login/oauth/access_token',oauth:oauth},function (e,r,body) {
-      this.body=r;
-      //console.log(body);
-      console.log(r);
-    })
+    
   }
 }
