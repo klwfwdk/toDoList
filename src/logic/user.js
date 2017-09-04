4da1f4d3013faa606420fce422326bf9042f2878
klwf <klwfwdk@qq.com>
2017-09-04 23:03:58 +0800

    new

--- src/logic/user.js
+++ src/logic/user.js
@@ -1,5 +1,9 @@
 module.exports = class extends think.Logic {
   indexAction() {
-
+var opt={
+  CONSUMER_KEY = "b7a21a56f032455afa67",
+  CONSUMER_SECRET = "c29bd25a4543859e69bb8b68b2e30afe705e98bf"
+}
+this.use(login(opt));
   }
 };
--- src/middleware/login.js
+++ src/middleware/login.js
@@ -1,5 +1,10 @@
 module.exports = (options, app) => {
+  const querystring = require('querystring');
+  const http = require('http');
+  /*const CONSUMER_KEY = "b7a21a56f032455afa67";
+  const CONSUMER_SECRET = "c29bd25a4543859e69bb8b68b2e30afe705e98bf";*/
   return (ctx, next) => {
+console.log(options.CONSUMER_SECRET);
     return next();
   };
 };
