290c2f8b45b1679695f9ad74ebc209b6c3567aca
klwf <klwfwdk@qq.com>
2017-09-04 10:18:50 +0800

    2w

--- src/controller/user.js
+++ src/controller/user.js
@@ -31,8 +31,9 @@ module.exports = class extends think.Controller {
    );
   
  }*/
-const CONSUMER_KEY="b7a21a56f032455afa67";
-const CONSUMER_SECRET="c29bd25a4543859e69bb8b68b2e30afe705e98bf";
+    const request = require('request');
+    const CONSUMER_KEY = "b7a21a56f032455afa67";
+    const CONSUMER_SECRET = "c29bd25a4543859e69bb8b68b2e30afe705e98bf";
     var qs = require('querystring')
       , oauth =
         {
