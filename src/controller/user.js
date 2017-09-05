0df79f1d700967a1c2d0df4c94b043aaeabdd71c
klwf <klwfwdk@qq.com>
2017-09-05 14:08:39 +0800

    2w

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,61 +1,61 @@
 const Base = require('./base.js');
 const Koa = require('koa');
 var app = new Koa;
-var http= require("http") 
-const querystring =require('querystring')
+var http = require("http")
+const querystring = require('querystring')
 //const login = require('login');
-const option ={
+const option = {
   CONSUMER_KEY: "b7a21a56f032455afa67",
-  CONSUMER_SECRET : "c29bd25a4543859e69bb8b68b2e30afe705e98bf"
+  CONSUMER_SECRET: "c29bd25a4543859e69bb8b68b2e30afe705e98bf"
 }
 module.exports = class extends Base {
   async loginAction() {
-    
-        let path = "http://github.com/login/oauth/authorize";
-        path += '?client_id=' + 'b7a21a56f032455afa67';
-        path += '&scope=' + 'user:email';
-        path += '&state=' + 'dataStr';
-        //转发到授权服务器
-        this.redirect(path);
-    
-      };
+    let path = "http://github.com/login/oauth/authorize";
+    path += '?client_id=' + 'b7a21a56f032455afa67';
+    path += '&scope=' + 'user:email';
+    path += '&state=' + 'dataStr';
+    //转发到授权服务器
+    this.redirect(path);
+
+  };
   async loginAfterAction(req, resp) {
-        var coded = this.ctx.param('code');
-        var state = this.ctx.param('state');
-        var headers = this.ctx.headers;
-       // console.log(headers);
-        var path = '/login/aouth/access_token';
-        headers.host = "github.com"
-        // console.log(coded);
-        var data = querystring.stringify({
-          client_id: option.CONSUMER_KEY,
-          client_secret: option.CONSUMER_SECRET,
-          code: coded
-        })
+    var coded = this.ctx.param('code');
+    var state = this.ctx.param('state');
+    var headers = this.ctx.headers;
+    // console.log(headers);
+    var path = '/login/aouth/access_token';
+    headers.host = "github.com"
+    // console.log(coded);
+    var data = querystring.stringify({
+      client_id: option.CONSUMER_KEY,
+      client_secret: option.CONSUMER_SECRET,
+      code: coded
+    })
     //console.log(contents);
-        var opts = {
-          host: "github.com",
-         // hostname: "github.com",
-          //port: '443',
-          path: path,
-          headers: {'Content-Type': 'application/x-www-form-urlencoded',
-          'Content-Length': Buffer.byteLength(data)
-  },
-          method: 'POST'
-        }
-        var req = http.request(opts, function (res) {
-          res.setEncoding('utf8');
-          res.on('data', function (data) {
-            //var args = data.split('&');
-            //var tokenInfo = args[0].split('=');
-            //var token = tokenInfo[1];
-            console.log("data",data);
-          })
-        }
-        );
-        req.on;
-        console.log('data');
-        req.write(data);
-        req.end;
-      }
+    var opts = {
+      host: "github.com",
+      // hostname: "github.com",
+      //port: '443',
+      path: path,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': Buffer.byteLength(data)
+      },
+      method: 'POST'
+    }
+    var req = http.request(opts, function (res) {
+      res.setEncoding('utf8');
+      res.on('data', function (data) {
+        //var tokenInfo = args[0].spl
+        //var args = data.split('&');it('=');
+        //var token = tokenInfo[1];
+        console.log("data", data);
+      })
+    }
+    );
+    req.on;
+    console.log('data');
+    req.write(data);
+    req.end;
+  }
 };
