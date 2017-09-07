8f5f87d97c9a25759055fc32da8d1e1df6223e33
klwf <klwfwdk@qq.com>
2017-09-07 19:09:46 +0800

    3w

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,7 @@ module.exports = {
         domain: '', 
         path: '/',
         maxAge: 10 * 3600 * 1000, // 10个小时
-        signed: false,
+       
         keys: [] // 当 signed 为 true 时，使用 keygrip 库加密时的密钥
       }
     
--- src/config/config.production.js
+++ src/config/config.production.js
@@ -5,7 +5,6 @@ module.exports = {
     domain: '', 
     path: '/',
     maxAge: 10 * 3600 * 1000, // 10个小时
-    signed: false,
     keys: [] // 当 signed 为 true 时，使用 keygrip 库加密时的密钥
   }
 
