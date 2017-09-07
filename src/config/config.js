8d51fdbdc9897662fe69bb7c82675b6feb916fcf
klwf <klwfwdk@qq.com>
2017-09-07 19:19:34 +0800

    9

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,3 @@
 // default config
 module.exports = {
-    cookie: {
-        domain: '', 
-        path: '/',
-        maxAge: 10 * 3600 * 1000, // 10个小时
-       
-        keys: [] // 当 signed 为 true 时，使用 keygrip 库加密时的密钥
-      }
-    
 };
--- src/config/config.production.js
+++ src/config/config.production.js
@@ -1,11 +1,4 @@
 // production config, it will load in production enviroment
 module.exports = {
   workers: 0 ,
-  cookie: {
-    domain: '', 
-    path: '/',
-    maxAge: 10 * 3600 * 1000, // 10个小时
-    keys: [] // 当 signed 为 true 时，使用 keygrip 库加密时的密钥
-  }
-
 };
